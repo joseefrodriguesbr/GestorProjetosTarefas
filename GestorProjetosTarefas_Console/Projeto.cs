4c5cfae243da9f3698f74106898e830ae9f4b4c1
joseefrodriguesbr <jose.e.f.rodrigues.br@gmail.com>
2025-05-15 00:30:42 -0300

    -

---
+++ GestorProjetosTarefas_Console/Projeto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorProjetosTarefas_Console
+{
+    internal class Projeto
+    {
+        public string Nome {  get; set; }
+
+        public string Detalhe { get; set; }
+
+        public double Orcamento { get; set; }
+
+        public Projeto(string nome, string detalhe, double orcamento)
+        {
+            Nome = nome;
+            Detalhe = detalhe;
+            Orcamento = orcamento;
+        }
+
+        public override string ToString()
+        {
+            return $@"Projeto: {Nome} , detalhe: {Detalhe}, orçamento : {Orcamento}";
+        }
+    }
+}
