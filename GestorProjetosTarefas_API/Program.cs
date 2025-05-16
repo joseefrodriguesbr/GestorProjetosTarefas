5b0e38bf450b7247f44e5ff3313bb4d62a96b05a
joseefrodriguesbr <jose.e.f.rodrigues.br@gmail.com>
2025-05-16 16:35:27 -0300

    Ajustes API

--- GestorProjetosTarefas_API/Program.cs
+++ GestorProjetosTarefas_API/Program.cs
@@ -1,6 +1,17 @@
+using System.Text.Json.Serialization;
+using GestorProjetosTarefas.Shared.Data.BD;
+using GestorProjetosTarefas_Console;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    var dal = new DAL<Empregado>();
+    return dal.Read();
+});
 
 app.Run();
