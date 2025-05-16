4774e44723456ef9dca8bc8a7a2ce1502cbcf247
joseefrodriguesbr <jose.e.f.rodrigues.br@gmail.com>
2025-05-16 16:03:57 -0300

    Adicionar arquivos de projeto.

---
+++ GestorProjetosTarefas.Shared.Data/BD/GestorProjetosTarefasContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GestorProjetosTarefas_Console;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestorProjetosTarefas.Shared.Data.BD
+{
+    public class GestorProjetosTarefasContext : DbContext
+    {
+        public DbSet<Empregado> Empregado { get; set; }
+
+        public DbSet<Tarefa> Tarefa { get; set; }
+
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=GestorProjetosTarefas_BD_V1;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        /*
+        public SqlConnection Connect()
+        {
+            return new SqlConnection(connectionString);
+        }
+        */
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString).UseLazyLoadingProxies();
+        }
+    }
+}
